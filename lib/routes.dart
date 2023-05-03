36a7875a8ec4afc5681bb2b62637881fee43567e
Palcomtech <admin@palcomtech.ac.id>
2023-05-03 13:49:10 +0700

    add transition animation

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,6 @@
 import 'package:belajar_navigasi/fifthscreen.dart';
 import 'package:belajar_navigasi/fourthscreen.dart';
+import 'package:belajar_navigasi/sliderightroute.dart';
 import 'package:flutter/material.dart';
 
 class RouteGenerator{
@@ -8,7 +9,8 @@ class RouteGenerator{
 
     switch (settings.name) {
       case '/fourth':
-        return MaterialPageRoute(builder: (_) => FourthScreen());
+        //return MaterialPageRoute(builder: (_) => FourthScreen());
+        return SlideRightRoute(widget: FourthScreen());
       case '/fifth':
         if(args is String){
           return MaterialPageRoute(builder: (_) => FifthScreen(data: args));
@@ -28,3 +30,4 @@ class RouteGenerator{
     });
   }  
 }
+
